5d228c68bf0a55c2c07ab675bc0215b0500afb33
kv1720 <63973759+kv1720@users.noreply.github.com>
2020-04-26 13:14:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function changeColor() {
 document.body.style.backgroundColor = "black";
 document.getElementById('dark-mode').style.display = "none";
-document.getElementById('light-mode').style.display = "block";
+document.getElementById('light-mode').style.display = "inline-block";
 }
 function changeBack() {
 document.body.style.backgroundColor = "indianred";
 
 var darkModeButton = document.getElementById('dark-mode');
-darkModeButton.style.display = "block";
+darkModeButton.style.display = "inline-block";
 
 var lightModeButton = document.getElementById('light-mode');
 lightModeButton.style.display = "none";
