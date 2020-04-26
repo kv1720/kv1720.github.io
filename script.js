4cd0bc9c3d8beedb96d3f66a87e6255156bff92b
kv1720 <63973759+kv1720@users.noreply.github.com>
2020-04-26 12:58:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function changeColor() {
 document.body.style.backgroundColor = "black";
+document.getElementById('dark-mode').style.display = "none";
+document.getElementById('light-mode').style.display = "block";
+}
+function changeBack() {
+document.body.style.backgroundColor = "indianred";
+
+var darkModeButton = document.getElementById('dark-mode');
+darkModeButton.style.display = "block";
+
+var lightModeButton = document.getElementById('light-mode');
+lightModeButton.style.display = "none";
 }
